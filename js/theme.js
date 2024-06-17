3609638db29e2c6ee8937717378793d5a1ef3b70
Mariia <79396737+MariiaMelychyn@users.noreply.github.com>
2024-06-17 22:17:48 +0300

    theme upload

--- js/theme.js
+++ js/theme.js
@@ -2,18 +2,21 @@ window.addEventListener("load", function () {
   console.log("thema");
   const savedTheme = localStorage.getItem("theme");
   console.log(savedTheme);
-
   if (savedTheme) {
     document.body.classList.add(savedTheme);
     document.getElementById("checkbox").checked = savedTheme === "dark";
+    document.getElementById("theme-icon").textContent =
+      savedTheme === "dark" ? "🌙" : "☀";
   } else {
     localStorage.setItem("theme", "light");
     document.body.classList.add("light");
+    document.getElementById("theme-icon").textContent = "☀";
   }
   document.getElementById("checkbox").addEventListener("change", function () {
     const isDarkMode = this.checked;
     localStorage.setItem("theme", isDarkMode ? "dark" : "light");
     document.body.classList.toggle("dark", isDarkMode);
     document.body.classList.toggle("light", !isDarkMode);
+    document.getElementById("theme-icon").textContent = isDarkMode ? "🌙" : "☀";
   });
 });
