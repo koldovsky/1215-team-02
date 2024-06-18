b405b43bc43680fa01e86a12872e0fd6c3317785
Mariia <79396737+MariiaMelychyn@users.noreply.github.com>
2024-06-18 16:48:03 +0300

    Update tasks02.js

--- js-tasks/tasks02.js
+++ js-tasks/tasks02.js
@@ -61,3 +61,7 @@ function sumArray(array) {
     array.splice(maxIndex, 1);
     return array.reduce((sum, num) => sum + num, 0);
 }
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+function makeUpperCase(str) {
+ return str.toUpperCase()
+}
