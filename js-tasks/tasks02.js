34a1808acb2081b4d1373d9fbfdd09227878cdb9
Mariia <79396737+MariiaMelychyn@users.noreply.github.com>
2024-06-18 16:41:22 +0300

    Update tasks02.js

--- js-tasks/tasks02.js
+++ js-tasks/tasks02.js
@@ -41,3 +41,9 @@ const stringToNumber = function(str){
  let number = parseInt(str);
     return number;
 }
+
+// https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/javascript
+function findNeedle(haystack) {
+ let index = haystack.indexOf("needle");
+    return `found the needle at position ${index}`;
+}
