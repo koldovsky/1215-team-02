eb835a4c4e7067d1e324bef24e89633b7f89f83e
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2024-06-19 15:06:39 +0300

    remove variant solving  "Opposite Attract" tasks02.js

--- js-tasks/tasks02.js
+++ js-tasks/tasks02.js
@@ -36,12 +36,6 @@ function lovefunc(flower1, flower2) {
   return (flower1 + flower2) % 2 !== 0;
 }
 
-// Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript    ВАРІАНТ
-function lovefunc(flower1, flower2){
-  if(flower1 % 2 === 0 && flower2 % 2 !== 0 || flower2 % 2 === 0 && flower1 % 2 !== 0) return true;
-  return false;
-}
-
 // https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
 const stringToNumber = function(str){
  let number = parseInt(str);
