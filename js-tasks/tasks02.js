d5a1863b09d1272953425e20e8ae65f9fbacd496
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2024-06-20 02:34:41 +0300

    additional task Is n divisible by x and y?

--- js-tasks/tasks02.js
+++ js-tasks/tasks02.js
@@ -45,6 +45,11 @@ function getRealFloor(n) {
   return n - 2;
 }
 
+// Is n divisible by x and y? https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0 ? true : false;
+}
+
 // https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
 const stringToNumber = function (str) {
   let number = parseInt(str);
