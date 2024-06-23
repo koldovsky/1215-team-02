87201db93dcbee55a14c44c2bf0d6e077b7367dd
Mariia <79396737+MariiaMelychyn@users.noreply.github.com>
2024-06-23 18:41:56 +0300

    Update tasks-03.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -60,3 +60,8 @@ class Dog extends Animal {
   }
 }
 //===========================================================
+
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
+function cockroachSpeed(s) {
+  return Math.floor(s * (100000 / 3600));
+}
