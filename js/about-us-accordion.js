0ac94ba235cfd03f7ecb3bcafade14760527734d
Mariia <79396737+MariiaMelychyn@users.noreply.github.com>
2024-06-28 18:27:40 +0300

    update

--- js/about-us-accordion.js
+++ js/about-us-accordion.js
@@ -3,13 +3,22 @@ const accordionHeaders = document.querySelectorAll(".accordion__header");
 accordionHeaders.forEach(header => {
     header.addEventListener("click", function() {
         const content = this.nextElementSibling;
-        content.classList.toggle("active");
+        const isActive = content.classList.toggle("active");
         this.classList.toggle("active");
-        
+
+        if (isActive) {
+            content.style.maxHeight = content.scrollHeight + 'px';
+        } else {
+            content.style.maxHeight = null;
+
+        }
+
         accordionHeaders.forEach(otherHeader => {
             if (otherHeader !== this) {
                 otherHeader.nextElementSibling.classList.remove("active");
                 otherHeader.classList.remove("active");
+                otherHeader.nextElementSibling.style.maxHeight = null;
+                otherHeader.nextElementSibling.style.padding = '0';
             }
         });
     });
