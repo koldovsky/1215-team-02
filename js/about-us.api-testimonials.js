fb4e9ee4e31a96009ee257c580b6bd66d3a067da
nadraharr <nadraharr@gmail.com>
2024-06-30 12:59:09 +0300

    api-testimonial carousel-starter

--- js/about-us.api-testimonials.js
+++ js/about-us.api-testimonials.js
@@ -1,33 +1,75 @@
 const response = await fetch("api/testimonials.json");
 const apiTestimonials = await response.json();
-const element = document.querySelector(".api-testimonials-carousel");
+const element = document.querySelector(".api-testimonials-carousel__inner");
+const buttonPrev = document.querySelector(".carousel__button--prev");
+buttonPrev.addEventListener("click", prevSlide)
+const buttonNext = document.querySelector(".carousel__button--next");
+buttonNext.addEventListener("click", nextSlide)
+let currentSlide = 0;
 renderApiTestimonials(element, apiTestimonials);
 
 async function renderApiTestimonials(element, testimonials) {
-  let name = "";
+  const formattedUsers = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+  ];
   try {
     const response = await fetch(
-      "https://fakerapi.it/api/v1/persons?_quantity=1"
+      "https://fakerapi.it/api/v1/persons?_quantity=5"
     );
-    const user = await response.json();
-    if (user.status === "OK") {
-      name = user.data[0].firstname + " " + user.data[0].lastname;
+    let users = await response.json();
+    if (response.ok && users.data) {
+      users = users.data;
+      for (let i = 0; i < 5; i++) {
+        formattedUsers[i].name = `${users[i].firstname} ${users[i].lastname}`;
+      }
     } else {
-      name = "John Doe";
+      for (let i = 0; i < 5; i++) {
+        formattedUsers[i].name = "John Doe";
+      }
     }
   } catch {
-    name = "John Doe";
+    for (let i = 0; i < 5; i++) {
+      formattedUsers[i].name = "John Doe";
+    }
   }
-  const image_url = "https://thispersondoesnotexist.com/";
-  const body =
-    testimonials[Math.floor(Math.random() * testimonials.length)].body;
-  element.innerHTML = `<article class="api-testimonials-carousel__item">
+
+  for (let user of formattedUsers) {
+    element.innerHTML += `<article class="api-testimonials-carousel__item">
         <div class="api-testimonials-carousel__image">
-            <img src="${image_url}" alt="Our client photo">
+            <img src="https://thispersondoesnotexist.com" alt="Our client photo">
         </div>
         <div class="api-testimonials-carousel__text">            
-            <p class="api-testimonials-carousel__text-body">"${body}"</p>
-            <p class="api-testimonials-carousel__text-name">&Tab;&mdash; ${name}</p>
+            <p class="api-testimonials-carousel__text-body">"${
+              testimonials[Math.floor(Math.random() * testimonials.length)].body
+            }"</p>
+            <p class="api-testimonials-carousel__text-name">&Tab;&mdash; ${
+              user.name
+            }</p>
         </div>
      </article>`;
+  }  
+  showButtons();
+}
+
+function prevSlide() {
+  const carouselPosition = `translateX(-${currentSlide * 100 - 100}%)`;
+  element.style.transform = carouselPosition;
+  currentSlide -= 1;
+  showButtons();
+}
+
+function nextSlide() {
+  const carouselPosition = `translateX(-${currentSlide * 100 + 100}%)`;
+  element.style.transform = carouselPosition;
+  currentSlide += 1;
+  showButtons();
+}
+
+function showButtons() {  
+  currentSlide > 0 ? buttonPrev.style.display = "block" : buttonPrev.style.display = "none";
+  currentSlide < 4 ? buttonNext.style.display = "block" : buttonNext.style.display = "none";
 }
